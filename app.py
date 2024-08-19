d1463f99412aef26b8e5a27192acff768c367d7b
sunghyun_nam <67773599+sunghyun-nam@users.noreply.github.com>
2024-08-19 11:17:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 from utils.get_cfg import get_cfg
-from utils.camerahandler import CameraHandler
 from utils.webhandler import WebHandler
+from utils.camerahandler import CameraHandler
+
+import os
 
 if __name__ == '__main__':
   cfg = get_cfg()
-	
+
+  if not os.path.exists(cfg['PI']['FILE_PATH']):
+    os.makedirs(cfg['PI']['FILE_PATH'])
+
   cam = CameraHandler(cfg)
   cam.loop()
   
