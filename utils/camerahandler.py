98b286cf1ba30fdb33d24f685d6883d7685ef4f8
sunghyun_nam <67773599+sunghyun-nam@users.noreply.github.com>
2024-09-02 20:28:39 +0900

    Update camerahandler.py

--- utils/camerahandler.py
+++ utils/camerahandler.py
@@ -13,12 +13,17 @@ class CameraHandler:
     def __init__(self, cfg):
         self.cfg = cfg
         
-        self.s_time = None
-        
+        self.sms_interval = None
+        self.fire_interval = None
+
         self.ir_frame = None
         self.rgb_frame = None
         
-        self.sms_flag = False
+        self.fire_limit = {
+            'min': 60,
+            'max': 140
+        }
+        self.fire_cont = None
         self.fire_flag = False
         
         self.picam2 = Picamera2()
@@ -29,21 +34,24 @@ class CameraHandler:
     def check_for_fire(self, data):
         temper = (data - 27315) / 100.0
         temper = temper.reshape(120, 160)
+        
         _, max_val, _, max_loc = cv2.minMaxLoc(temper)
-        if max_val >= 60 and max_val < 140:
-            if self.s_time is None:
-                self.s_time = time.time()
-            elif time.time() - self.s_time >= 3:
+        if max_val >= self.fire_limit['min'] and max_val < self.fire_limit['max']:
+            mask = temper > self.fire_limit['min']
+            self.fire_cont, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            if self.fire_interval is None:
+                self.fire_interval = time.time()
+            elif time.time() - self.fire_interval >= 3:
                 self.fire_flag = True
-                if self.sms_flag:
+                if self.sms_interval is None or time.time() - self.sms_interval >= 300:
                     send_sms()
-                    self.sms_flag = False
+                    self.sms_interval = time.time()
         else:
-            self.s_time = None
             self.fire_flag = False
-            
+            self.fire_interval = None
+        
         return "{:.2f}C".format(max_val), max_loc
-    
+        
     def capture(self):
         rgb = self.picam2.capture_array("main")
         with Lepton3(self.cfg['PI']['SERIAL_PORT']) as l:
@@ -67,6 +75,15 @@ class CameraHandler:
         cv2.line(frame, (x, y + 2), (x, y + 10), (255, 255, 255), 1)
         cv2.circle(frame, (x, y), 2, (255, 255, 255), 1)
         cv2.putText(frame, val, (x + 10, y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (255, 255, 255), 1)
+        
+        if self.fire_cont is not None:
+            for contour in self.fire_cont:
+                x, y, w, h = cv2.boundingRect(contour)
+        
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+                cv2.putText(frame, "fire", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0, 0, 255), 1)
+        
+            self.fire_cont = None
         
         return frame
 
